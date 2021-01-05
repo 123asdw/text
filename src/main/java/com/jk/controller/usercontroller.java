859a729ab4fb10c3973ae7dafbd7f057ddca2275
Your Name <3474458278@qq.com>
2021-01-05 17:11:04 +0800

    地产

---
+++ src/main/java/com/jk/controller/usercontroller.java
@@ -0,0 +1,24 @@
+package com.jk.controller;
+
+/**
+ * Package: com.jk.controller
+ * <p>
+ * Description： TODO
+ * <p>
+ * Author: zxw
+ * <p>
+ * Date: Created in 2021/1/5 17:02
+ * <p>
+ * Company: 11
+ * <p>
+ * Copyright: Copyright (c) 2017
+ * <p>
+ * Version: 0.0.1
+ * <p>
+ * Modified By:
+ */
+public class usercontroller {
+    public static void main(String[] args) {
+        System.out.println(4444444);
+    }
+}
